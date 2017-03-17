d12a6aacb86a4d48e28f0de1e69ca44fe21897f6
david <david@DESKTOP-6KUKONL>
2017-03-16 21:08:08 -0400

    Add Test Project and blank unit test

---
+++ TDDUnitTest/TDD.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TDDUnitTest
+{
+    [TestClass]
+    public class TDD
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
