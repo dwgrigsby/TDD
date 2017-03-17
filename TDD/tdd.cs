add73d67fe6199e11b876e9645a781bcc088a622
david <david@DESKTOP-6KUKONL>
2017-03-16 22:14:24 -0400

    clean up from merge

--- TDD/tdd.cs
+++ TDD/tdd.cs
@@ -17,27 +17,9 @@ namespace TDD
             InitializeComponent();
         }
 
-        private void label1_Click(object sender, EventArgs e)
-        {
 
-        }
 
-        private void label3_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox5_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label6_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button1_Click(object sender, EventArgs e)
+        private void CallDSAPI_Click(object sender, EventArgs e)
         {
 
         }
